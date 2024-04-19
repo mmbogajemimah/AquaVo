6705fb284befc116f80b3b35b96965dc71da5c80
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-04-20 00:56:13 +0300

    Added Update Refills by URL ID urlpattern

--- aquavo_backend/refills/urls.py
+++ aquavo_backend/refills/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('create_refill/', views.CreateRefillView.as_view(), name='create_refill'),
     path('all_refills/', views.GetAllRefillsView.as_view(), name="all_refills"),
     path('user_refills/<int:customer_id>/', views.GetRefillsForUserView.as_view(), name='user_refills'),
+    path('refill_by_id/<int:refill_id>/', views.GetRefillByIdView.as_view(), name='refill_by_id'),
     path('update_refills/<int:refill_id>/', views.UpdateRefillView.as_view(), name='update_refill'),
     path('delete_refills/<int:refill_id>/', views.DeleteRefillView.as_view(), name='delete_refill'),
 ]
