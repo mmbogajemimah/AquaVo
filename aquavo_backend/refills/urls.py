93a8a27dfd873998495e6c4343b05a70534da76b
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-04-19 09:52:17 +0300

    Add Update URL

--- aquavo_backend/refills/urls.py
+++ aquavo_backend/refills/urls.py
@@ -6,6 +6,9 @@ app_name = 'refills'
 
 urlpatterns = [
     path('create_refill/', views.CreateRefillView.as_view(), name='create_refill'),
+    path('all_refills/', views.GetAllRefillsView.as_view(), name="all_refills"),
+    path('user_refills/<int:customer_id>/', views.GetRefillsForUserView.as_view(), name='user_refills'),
+    path('update_refills/<int:refill_id>/', views.UpdateRefillView.as_view(), name='update_refill'),
 ]
 
 
