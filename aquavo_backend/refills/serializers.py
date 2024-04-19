1b282be4d3dca737e023b8809ac7d1a9c214fbd6
Jemimah Mmboga <mmbogajemimah@gmail.com>
2024-04-20 00:54:46 +0300

    Added Customer details from CustomerSerializer to RefillSerializer

--- aquavo_backend/refills/serializers.py
+++ aquavo_backend/refills/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Refill
+from accounts.models import CustomUser
+from accounts.serializers import CustomerSerializer
 
 class RefillSerializer(serializers.ModelSerializer):
+    customer = CustomerSerializer(read_only = True)
     class Meta:
         model = Refill
         fields = ['id', 'customer', 'amount_liters', 'amount_money', 'created_at']
